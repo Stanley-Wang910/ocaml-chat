027b57d59b9ffd97d03db171b5c10a7a9ab4d5b0
twinfantasyfan <wangstanley910@gmail.com>
2024-11-20 18:43:23 -0500

    get username working async user registration

--- bin/client.ml
+++ bin/client.ml
@@ -18,29 +18,53 @@ let send_message oc msg =
 let receive_message ic =
   Lwt_io.read_line_opt ic
 
-let rec input_loop oc =
-  let* () = Lwt_io.write Lwt_io.stdout "> " in
-  let* input = Lwt_io.read_line Lwt_io.stdin in
-  match input with
-  | "quit" -> Lwt_io.close oc
-  | msg ->
-    let* () = send_message oc msg in
-    input_loop oc
+
+let clear_line = "\027[2K"
+let move_cursor_left = "\027[1000D"
+
+let rec input_loop oc input_buffer =
+ let* char = Lwt_io.read_char Lwt_io.stdin in
+  match char with
+  | '\n' ->
+    let input = Buffer.contents input_buffer in
+    Buffer.clear input_buffer;
+    if input = "quit" then
+      Lwt_io.close oc
+    else
+      let* () = send_message oc input in
+      input_loop oc input_buffer
+  | c ->
+    Buffer.add_char input_buffer c;
+    let* () = Lwt_io.write_char Lwt_io.stdout c in
+    Lwt_io.flush Lwt_io.stdout >>= fun () ->
+    input_loop oc input_buffer
     
 
-let rec receive_loop ic = 
-    let* response = receive_message ic in
-    match response with
-    | Some resp ->
-        let* () = Lwt_io.write_line Lwt_io.stdout ("\n" ^ resp) in
-        let* () = Lwt_io.write Lwt_io.stdout "> " in
-        receive_loop ic
-    | None ->
-      Lwt_io.write_line Lwt_io.stdout "Server disconnected"
+let rec receive_loop ic input_buffer =
+  let* response = receive_message ic in
+  match response with
+  | Some resp ->
+      let current_input = Buffer.contents input_buffer in
+      (* Move cursor to the beginning of the line and clear it *)
+      let* () = Lwt_io.write Lwt_io.stdout (move_cursor_left ^ clear_line) in
+      (* Print the incoming message *)
+      let* () = Lwt_io.write_line Lwt_io.stdout resp in
+      (* Re-display the prompt and current input *)
+      let* () = Lwt_io.write Lwt_io.stdout ("> " ^ current_input) in
+      (* Flush stdout to ensure everything is displayed *)
+      let* () = Lwt_io.flush Lwt_io.stdout in
+      receive_loop ic input_buffer
+  | None ->
+    Lwt_io.write_line Lwt_io.stdout "Server disconnected"
 
 let () =
   Lwt_main.run begin
     let* (ic, oc) = create_connection () in
-    let* () = Lwt_io.write_line Lwt_io.stdout "Connected to server. Type 'quit' to exit.\n" in
-    Lwt.pick [input_loop oc ; receive_loop ic]
+    let* () = Lwt_io.write_line Lwt_io.stdout "Connected to server. Please enter your username. Type 'quit' to exit." in
+    let* () = Lwt_io.write Lwt_io.stdout "\nUsername: " in
+    let input_buffer = Buffer.create 128 in
+    Lwt.pick [
+      input_loop oc input_buffer;
+      receive_loop ic input_buffer
+    ]
   end
--- bin/server.ml
+++ bin/server.ml
@@ -1,79 +1,94 @@
 (*open Lwt*)
 open Lwt_unix
 open Lwt.Syntax
-open Lwt.Infix
+(*open Lwt.Infix*)
 
 (**)
-(*type client = { *)
-(*  ic: Lwt_io.input_channel;*)
-(*  oc: Lwt_io.output_channel;*)
-(*  addr: Lwt_unix.sockaddr;*)
-(*  username: string;*)
-(*}*)
+type client = { 
+  ic: Lwt_io.input_channel;
+  oc: Lwt_io.output_channel;
+  id: int;
+  username: string;
+}
 
-let counter = ref 0
+(*let counter = ref 0*)
 let listen_address = Unix.inet_addr_loopback
 let port = 9000
-let backlog = 10
+let backlog = 5
 
 (* Keep track of all connected clients *)
+let client_id_counter = ref 0
 let clients = ref []
 
 (* Broadcast a message to all connected clients *)
-let broadcast msg client =
-  Lwt_list.iter_p
-    (fun oc ->
+let broadcast msg sender =
+  Lwt_list.map_p
+    (fun client ->
+      if client.id <> sender.id then
       Lwt.catch
-        (fun () -> if oc != client then Lwt_io.write_line oc msg else Lwt_io.write_line oc "you are messenger")
-        (fun _ -> Lwt.return_unit))
+        (fun () -> Lwt_io.write_line client.oc (Printf.sprintf "\n%s > %s\n" sender.username msg))
+        (fun _ -> Lwt.return_unit)
+      else
+      (*Lwt.catch*)
+      (*  (fun () -> Lwt_io.write_line client.oc (Printf.sprintf "\nyou > %s\n" msg))*)
+        Lwt_io.write_line sender.oc ("\nsent\n")
+        )
     !clients
 
-(* Server input handling loop *)
-let rec server_input_loop () =
-  let* () = Lwt_io.read_line_opt Lwt_io.stdin >>= function
-    | Some "quit" -> Lwt.fail Exit
-    | Some msg ->
-        let* () = Lwt_io.write_line Lwt_io.stdout ("Server: " ^ msg) in
-        Printf.printf "Broadcasted: %s\n" msg;
-        Lwt.return_unit
-    | None -> Lwt.return_unit
-  in
-  server_input_loop ()
+(*let handle_message msg = *)
+(*  match msg with*)
+(*  | "read" -> string_of_int !counter*)
+(*  | "inc" -> counter := !counter + 1; "Incremented Counter"*)
+(*  | _ -> msg*)
 
-let handle_message msg = 
-  match msg with
-  | "read" -> string_of_int !counter
-  | "inc" -> counter := !counter + 1; "Incremented Counter"
-  | _ -> Printf.sprintf "Client says: %s" msg
-
-let rec handle_connection ic oc =
-  let* msg = Lwt_io.read_line_opt ic in
+let rec handle_connection client =
+  let* msg = Lwt_io.read_line_opt client.ic in
   match msg with
   | Some msg ->
-    let reply = handle_message msg in
-    let* () = broadcast reply oc in
-    handle_connection ic oc
+    let _ = broadcast msg client in
+    handle_connection client
   | None -> 
-    clients := List.filter (fun c -> c != oc) !clients;
-    Printf.printf "Client disconnected. Total clients: %d\n" (List.length !clients);
+    clients := List.filter (fun c -> c.id <> client.id) !clients;
+    Printf.printf "Client %d disconnected. Total clients: %d\n" client.id (List.length !clients);
     Lwt.return_unit
+
 
 let accept_connection conn =
   let fd, _ = conn in
   let ic = Lwt_io.of_fd ~mode:Lwt_io.Input fd in
   let oc = Lwt_io.of_fd ~mode:Lwt_io.Output fd in
-
-  clients := oc :: !clients;
-  let* () = Lwt_io.write_line Lwt_io.stdout (Printf.sprintf "New client connected. Total clients: %d\n" (List.length !clients)) in
+  let id = !client_id_counter in
+  incr client_id_counter;
   Lwt.async (fun () -> 
+    let* username = Lwt_io.read_line ic in
+    let* () = Lwt_io.write_line oc (Printf.sprintf "Welcome to the chat %s!" username) in  (* Changed this line *)
+    let client = { ic; oc; id ; username } in 
+    clients := client :: !clients;
+    Printf.printf "New client %d connected. Total clients: %d\n" client.id (List.length !clients);
+  
     Lwt.catch
-      (fun () -> handle_connection ic oc)
+      (fun () -> handle_connection client)
       (fun _ -> 
-        clients := List.filter (fun c -> c != oc) !clients;
+        clients := List.filter (fun c -> c.id <> client.id) !clients;
         Lwt.return_unit
       )
   );
   Lwt.return_unit
+  
+(*(* Server input handling loop *)*)
+(*let rec server_input_loop () =*)
+(*  let* () = Lwt_io.read_line_opt Lwt_io.stdin >>= function*)
+(*    | Some "quit" -> Lwt.fail Exit*)
+(*    | Some msg ->*)
+(*        let* () = Lwt_io.write_line Lwt_io.stdout ("Server: " ^ msg) in*)
+(*        Printf.printf "Broadcasted: %s\n" msg;*)
+(*        Lwt.return_unit*)
+(*    | None -> Lwt.return_unit*)
+(*  in*)
+(*  server_input_loop ()*)
+
+
+
 
 let create_socket () =
   let sock = Lwt_unix.(socket PF_INET SOCK_STREAM 0) in
@@ -93,6 +108,6 @@ let () =
   Lwt_main.run
     (Lwt.pick [
       create_server sock ();
-      server_input_loop ()
+      (*server_input_loop ()*)
     ])
 
